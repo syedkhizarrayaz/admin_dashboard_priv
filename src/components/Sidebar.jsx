5d6f86a79dcbd0aab44ee6970a92727c2827dbac
Syed Khizar Rayaz <khizar.rayaz98@gmail.com>
2024-05-06 01:19:40 +0500

    table bugs fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,59 +1,118 @@
-import React from 'react'
+// import React from 'react'
+// import { Link, NavLink } from 'react-router-dom';
+// import { SiShopware } from 'react-icons/si';
+// import { MdOutlineCancel } from 'react-icons/md';
+// import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+// import { links } from '../data/dummy';
+// import { useStateContext } from '../contexts/ContextProvider';
+
+// const Sidebar = () => {
+//   const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+//   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+//   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+//   const handleCloseSideBar = () => {
+//     if (activeMenu !== undefined && screenSize <= 900) {
+//       setActiveMenu(true);
+      
+//     }
+//   };
+//   return (
+//     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+//       {activeMenu && (
+//         <>
+//           <div className="flex justify-between items-center">
+//             <Link to="/Users" 
+//             // onClick={handleCloseSideBar} 
+//             className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-white">
+//               <SiShopware /> <span>Admin Panel</span>
+//             </Link>
+//             <TooltipComponent content="Menu" position="BottomCenter">
+//               <button
+//                 type="button"
+//                 onClick={() => setActiveMenu(!activeMenu)}
+//                 style={{ color: 'black', borderRadius: '50%'}}
+//                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+//               >
+//                 <MdOutlineCancel />
+//               </button>
+//             </TooltipComponent>
+//           </div>
+//           <div className="mt-10 ">
+//             {links.map((item) => (
+//               <div key={item.title}>
+//                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-3 uppercase">
+//                   {item.title}
+//                 </p>
+//                 {item.links.map((link) => (
+//                   <NavLink
+//                     to={`/${link.name}`}
+//                     key={link.name}
+//                     // onClick={handleCloseSideBar}
+//                     style={({ isActive }) => ({
+//                     })}
+//                     className={({ isActive }) => (isActive ? activeLink : normalLink)}
+//                   >
+//                     {link.icon}
+//                     <span className="capitalize ">{link.name}</span>
+//                   </NavLink>
+//                 ))}
+//               </div>
+//             ))}
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default Sidebar
+
+import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
+import { links } from '../data/dummy';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
-  const handleCloseSideBar = () => {
-    if (activeMenu !== undefined && screenSize <= 900) {
-      setActiveMenu(true);
-      
+  const { screenSize, activeMenu, setActiveMenu } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
     }
   };
+
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/Users" 
-            // onClick={handleCloseSideBar} 
-            className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-white">
-              <SiShopware /> <span>Admin Panel</span>
+            <Link to="/Users" className="flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight text-white" onClick={handleCloseSidebar}>
+              <SiShopware size={24} /> <span>Admin Panel</span>
             </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
-              <button
-                type="button"
-                onClick={() => setActiveMenu(!activeMenu)}
-                style={{ color: 'black', borderRadius: '50%'}}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-              >
-                <MdOutlineCancel />
-              </button>
-            </TooltipComponent>
+            <button
+              type="button"
+              onClick={() => setActiveMenu(!activeMenu)}
+              className="text-xl p-2 md:p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <MdOutlineCancel size={24} />
+            </button>
           </div>
-          <div className="mt-10 ">
+          <div className="mt-8">
             {links.map((item) => (
               <div key={item.title}>
-                <p className="text-gray-400 dark:text-gray-400 m-3 mt-3 uppercase">
-                  {item.title}
-                </p>
+                <p className="text-gray-400 m-3 mt-3 uppercase">{item.title}</p>
                 {item.links.map((link) => (
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    // onClick={handleCloseSideBar}
-                    style={({ isActive }) => ({
-                    })}
-                    className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                    onClick={handleCloseSidebar}
+                    className="flex items-center gap-3 pl-4 pt-3 pb-2.5 text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 rounded-lg"
+                    activeClassName="text-white"
                   >
                     {link.icon}
-                    <span className="capitalize ">{link.name}</span>
+                    <span className="capitalize">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
@@ -62,7 +121,7 @@ const Sidebar = () => {
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,3 +1,124 @@
+// import React, { useRef, useEffect, useState } from 'react';
+// import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+// import { ClipLoader } from 'react-spinners';
+// import { Header } from '../components';
+// import Switch from 'react-switch';
+
+// const Users = () => {
+//   const selectionsettings = { persistSelection: true };
+
+//   const gridInstance = useRef(null);
+//   const [usersData, setUsersData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetchUsersList();
+//   }, []); // Fetch data on component mount
+
+//   const fetchUsersList = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3001/api/users'); // Fetch data from backend API
+//       if (response.ok) {
+//         const result = await response.json();
+//         setUsersData(result);
+//       } else {
+//         console.error('Failed to fetch users list');
+//       }
+//     } catch (error) {
+//       console.error('Error fetching users list:', error.message);
+//     } finally {
+//       setLoading(false); // Set loading to false regardless of success or failure
+//     }
+//   };
+
+//   const handleSwitchChange = async (checked, columnName, rowIndex) => {
+//     try {
+//       const userToUpdate = usersData[rowIndex];
+//       if (userToUpdate && columnName) {
+//         console.log('columnName:', columnName);
+//         console.log('checked:', checked);
+        
+//         // Construct request body
+//         const requestBody = {
+//           virus_total: usersData[rowIndex].virus_total,
+//           the_phish: usersData[rowIndex].the_phish,
+//           sms_whatsapp_phishing: usersData[rowIndex].sms_whatsapp_phishing,
+//           [columnName]: checked ? 1 : 0
+//         };
+//         console.log('requestBody:', requestBody);
+  
+//         const response = await fetch(`http://localhost:3001/api/users/${userToUpdate.id}`, {
+//           method: 'PUT',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify(requestBody)
+//         });
+//         if (response.ok) {
+//           const updatedUsersData = [...usersData];
+//           updatedUsersData[rowIndex][columnName] = checked ? 1 : 0;
+//           setUsersData(updatedUsersData);
+//         } else {
+//           console.error('Failed to update user status');
+//         }
+//       } else {
+//         console.error('Invalid columnName or userToUpdate');
+//       }
+//     } catch (error) {
+//       console.error('Error updating user status:', error.message);
+//     }
+//   };
+    
+//   return (
+//     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-main-dark-bg rounded-3xl">
+//       <Header category="Page" title="Users" />
+//       {loading ? (
+//         // Render loader when data is still loading
+//         <div className="flex justify-center items-center h-screen bg-secondary-dark-bg">
+//           <ClipLoader color="#fff" size={100} />
+//         </div>
+//       ) : (
+//         // Render the grid once data is loaded
+//         <GridComponent
+//           dataSource={usersData}
+//           allowPaging
+//           pageSettings={{ pageCount: 5 }}
+//           selectionSettings={selectionsettings}
+//           allowSorting
+//           ref={gridInstance}
+//         >
+//           <ColumnsDirective>
+//             <ColumnDirective field="id" headerText="User ID" isPrimaryKey={true} />
+//             <ColumnDirective field="username" headerText="Username" />
+//             <ColumnDirective field="name" headerText="Name" />
+//             <ColumnDirective field="virus_total" headerText="Virus Total" template={(rowData) => (
+//               <Switch
+//                 onChange={(checked) => handleSwitchChange(checked, 'virus_total', rowData.index)}
+//                 checked={rowData.virus_total === 1}
+//               />
+//             )} />
+//             <ColumnDirective field="the_phish" headerText="The Phish" template={(rowData) => (
+//               <Switch
+//                 onChange={(checked) => handleSwitchChange(checked, 'the_phish', rowData.index)}
+//                 checked={rowData.the_phish === 1}
+//               />
+//             )} />
+//             <ColumnDirective field="sms_whatsapp_phishing" headerText="SMS & Whatsapp Phishing" template={(rowData) => (
+//               <Switch
+//                 onChange={(checked) => handleSwitchChange(checked, 'sms_whatsapp_phishing', rowData.index)}
+//                 checked={rowData.sms_whatsapp_phishing === 1}
+//               />
+//             )} width={200}/>
+//             <ColumnDirective field="payment_date" headerText="Payment" width={120}/>
+//           </ColumnsDirective>
+//           <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+//         </GridComponent>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Users;
 import React, { useRef, useEffect, useState } from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
 import { ClipLoader } from 'react-spinners';
@@ -31,22 +152,17 @@ const Users = () => {
     }
   };
 
-  const handleSwitchChange = async (checked, columnName, rowIndex) => {
+  const handleSwitchChange = async (checked, columnName, userId) => {
     try {
-      const userToUpdate = usersData[rowIndex];
+      const userToUpdate = usersData.find(user => user.id === userId);
       if (userToUpdate && columnName) {
-        console.log('columnName:', columnName);
-        console.log('checked:', checked);
-        
-        // Construct request body
         const requestBody = {
-          virus_total: usersData[rowIndex].virus_total,
-          the_phish: usersData[rowIndex].the_phish,
-          sms_whatsapp_phishing: usersData[rowIndex].sms_whatsapp_phishing,
+          virus_total: userToUpdate.virus_total,
+          the_phish: userToUpdate.the_phish,
+          sms_whatsapp_phishing: userToUpdate.sms_whatsapp_phishing,
           [columnName]: checked ? 1 : 0
         };
-        console.log('requestBody:', requestBody);
-  
+
         const response = await fetch(`http://localhost:3001/api/users/${userToUpdate.id}`, {
           method: 'PUT',
           headers: {
@@ -55,8 +171,12 @@ const Users = () => {
           body: JSON.stringify(requestBody)
         });
         if (response.ok) {
-          const updatedUsersData = [...usersData];
-          updatedUsersData[rowIndex][columnName] = checked ? 1 : 0;
+          const updatedUsersData = usersData.map(user => {
+            if (user.id === userToUpdate.id) {
+              return { ...user, [columnName]: checked ? 1 : 0 };
+            }
+            return user;
+          });
           setUsersData(updatedUsersData);
         } else {
           console.error('Failed to update user status');
@@ -68,7 +188,32 @@ const Users = () => {
       console.error('Error updating user status:', error.message);
     }
   };
-    
+
+  useEffect(() => {
+    if (gridInstance.current) {
+      adjustColumnWidths();
+    }
+  }, [usersData]); // Adjust column widths whenever data changes
+
+  const adjustColumnWidths = () => {
+    if (gridInstance.current) {
+      const columns = gridInstance.current.columns;
+      columns.forEach(column => {
+        const columnName = column.field;
+        let maxWidth = 0;
+        usersData.forEach(row => {
+          const content = row[columnName] ? String(row[columnName]) : ''; // Get content of the cell
+          const contentWidth = content.length * 8; // Adjust the multiplier as needed based on font size and style
+          if (contentWidth > maxWidth) {
+            maxWidth = contentWidth;
+          }
+        });
+        column.width = maxWidth > 200 ? maxWidth : 200; // Set minimum width for the column
+      });
+      gridInstance.current.refreshColumns();
+    }
+  };
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-main-dark-bg rounded-3xl">
       <Header category="Page" title="Users" />
@@ -88,28 +233,28 @@ const Users = () => {
           ref={gridInstance}
         >
           <ColumnsDirective>
-            <ColumnDirective field="id" headerText="User ID" isPrimaryKey={true} />
-            <ColumnDirective field="username" headerText="Username" />
-            <ColumnDirective field="name" headerText="Name" />
+            <ColumnDirective field="id" headerText="User ID" isPrimaryKey={true} width={100} />
+            <ColumnDirective field="username" headerText="Username" width={150} />
+            <ColumnDirective field="name" headerText="Name" width={150} />
             <ColumnDirective field="virus_total" headerText="Virus Total" template={(rowData) => (
               <Switch
-                onChange={(checked) => handleSwitchChange(checked, 'virus_total', rowData.index)}
+                onChange={(checked) => handleSwitchChange(checked, 'virus_total', rowData.id)}
                 checked={rowData.virus_total === 1}
               />
-            )} />
+            )} width={150} />
             <ColumnDirective field="the_phish" headerText="The Phish" template={(rowData) => (
               <Switch
-                onChange={(checked) => handleSwitchChange(checked, 'the_phish', rowData.index)}
+                onChange={(checked) => handleSwitchChange(checked, 'the_phish', rowData.id)}
                 checked={rowData.the_phish === 1}
               />
-            )} />
+            )} width={150} />
             <ColumnDirective field="sms_whatsapp_phishing" headerText="SMS & Whatsapp Phishing" template={(rowData) => (
               <Switch
-                onChange={(checked) => handleSwitchChange(checked, 'sms_whatsapp_phishing', rowData.index)}
+                onChange={(checked) => handleSwitchChange(checked, 'sms_whatsapp_phishing', rowData.id)}
                 checked={rowData.sms_whatsapp_phishing === 1}
               />
-            )} width={200}/>
-            <ColumnDirective field="payment_date" headerText="Payment" width={120}/>
+            )} width={200} />
+            <ColumnDirective field="payment_date" headerText="Payment" width={150} />
           </ColumnsDirective>
           <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
         </GridComponent>
